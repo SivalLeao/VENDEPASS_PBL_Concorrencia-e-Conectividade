81dae296f5859a807b69db4cd82911cf0dfc4c51
avcsilva <avcsilva@ecomp.uefs.br>
2024-10-15 18:21:21 -0300

    feat: novos endpoints
    Adiciona requisições para cadastro, compra, cancelamento e visualização de rotas. Além disso, torna o cliente utilizável por meio de terminal

--- Fase 2 - Servidores Distribuídos/Client/cliente.go
+++ Fase 2 - Servidores Distribuídos/Client/cliente.go
@@ -4,59 +4,250 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 )
 
-var url string = "http://localhost:8080"
+var url string // = "http://localhost:8080"
 
-func main() {
-	// Requisição GET
-	response, err := http.Get(url+"/rotas")
+// Estrutura de requisição de cliente (presente no servidor)
+type Client_req struct {
+	Id int `json:"id"`
+	Rota string `json:"rota"`
+}
+
+// Estrutura de requisição de cadastro (presente no servidor)
+type Cadastro_req struct{
+	Id int `json:"id"`
+	Nome string `json:"nome"`
+}
+
+// Função para requisição POST de cadastro com retorno de ID
+func cadastrar(nome string) int {
+	var cadastro = Cadastro_req{Nome: nome}
+	json_valor, err := json.Marshal(cadastro)
+	if err != nil {
+		fmt.Println("Erro ao serializar o JSON:", err)
+		return -1
+	}
+
+	resposta, err := http.Post(url+"/cadastro", "application/json", bytes.NewBuffer(json_valor))
+	if err != nil {
+		fmt.Println("Erro ao fazer a requisição POST:", err)
+		return -1
+	}
+	defer resposta.Body.Close()
+
+	var resposta_map map[string]interface{}
+	if err := json.NewDecoder(resposta.Body).Decode(&resposta_map); err != nil {
+		fmt.Println("Erro ao decodificar o JSON:", err)
+		return -1
+	}
+
+	id, ok := resposta_map["id"].(float64)
+	if !ok {
+		fmt.Println("Erro ao converter o ID")
+		return -1
+	}
+
+	return int(id)
+}
+
+// Função para requisição GET de rotas gerais com retorno de map
+func rotas_todas() map[string]int {
+	resposta, err := http.Get(url+"/rotas")
 	if err != nil {
 		fmt.Println("Erro ao fazer a requisição GET:", err)
-		return
+		return nil
 	}
-	defer response.Body.Close()
+	defer resposta.Body.Close()
 
-	// body, err := io.ReadAll(response.Body)
-	// if err != nil {
-	// 	fmt.Println("Erro ao ler o corpo da resposta:", err)
-	// 	return
-	// }
+	var resposta_map map[string]interface{}
 
-	// fmt.Println("Resposta GET:", string(body))
-
-	var rotas_recebidas map[string]int
-	if err := json.NewDecoder(response.Body).Decode(&rotas_recebidas); err != nil {
+	if err := json.NewDecoder(resposta.Body).Decode(&resposta_map); err != nil {
 		fmt.Println("Erro ao decodificar o JSON:", err)
-		return
+		return nil
 	}
 
-	fmt.Println("Quantidade de rotas recebidas:", len(rotas_recebidas))
-	fmt.Println("Rotas recebidas:", rotas_recebidas)
+    rotas_interface, ok := resposta_map["rotas"].(map[string]interface{})
+    if !ok {
+        fmt.Println("Erro ao converter as rotas")
+        return nil
+    }
 
-	// Requisição POST
-	jsondata := map[string]string{
-		"message": "Hello, World!"}
-	jsonvalue, err := json.Marshal(jsondata)
+    rotas_recebidas := make(map[string]int)
+    for rota, pertence := range rotas_interface {
+        if valor, ok := pertence.(float64); ok {
+            rotas_recebidas[rota] = int(valor)
+        } else {
+            fmt.Println("Erro ao converter o valor da rota:", rota)
+        }
+    }
+
+	return rotas_recebidas
+}
+
+// Função para requisição GET de rotas do cliente com retorno de slice
+func rotas_cliente(id int) []string {
+	resposta, err := http.Get(fmt.Sprintf("%s/rotas_cliente?id=%d", url, id))
+	if err != nil {
+		fmt.Println("Erro ao fazer a requisição GET:", err)
+		return nil
+	}
+	defer resposta.Body.Close()
+
+	var resposta_map map[string]interface{}
+	if err := json.NewDecoder(resposta.Body).Decode(&resposta_map); err != nil {
+		fmt.Println("Erro ao decodificar o JSON:", err)
+		return nil
+	}
+
+	rotas_interface, ok := resposta_map["rotas"].([]interface{})
+	if !ok {
+		resp, ok := resposta_map["error"].(string)
+		if ok {
+			fmt.Println(resp)
+			return nil
+		}
+		fmt.Println("Erro ao converter as rotas")
+		return nil
+	}
+
+	rotas_recebidas := make([]string, len(rotas_interface))
+	for i, rota := range rotas_interface {
+		if valor, ok := rota.(string); ok {
+			rotas_recebidas[i] = valor
+		} else {
+			fmt.Println("Erro ao converter o valor da rota:", rota)
+		}
+	}
+
+	return rotas_recebidas
+}
+
+// Função para requisição PATCH de compra de rota
+func comprar(id int, rota string) {
+	var cliente = Client_req{Id: id, Rota: rota}
+	json_valor, err := json.Marshal(cliente)
 	if err != nil {
 		fmt.Println("Erro ao serializar o JSON:", err)
 		return
 	}
 
-	response, err = http.Post(url+"/broadcast", "application/json", bytes.NewBuffer(jsonvalue))
+	req, err := http.NewRequest("PATCH", url+"/comprar_rota", bytes.NewBuffer(json_valor))
 	if err != nil {
-		fmt.Println("Erro ao fazer a requisição POST:", err)
+		fmt.Println("Erro ao fazer a requisição PATCH:", err)
 		return
 	}
-	defer response.Body.Close()
+	req.Header.Set("Content-Type", "application/json")
 
-	body, err := io.ReadAll(response.Body)
+	cliente_http := &http.Client{}
+	resposta, err := cliente_http.Do(req)
 	if err != nil {
-		fmt.Println("Erro ao ler o corpo da resposta:", err)
+		fmt.Println("Erro ao fazer a requisição PATCH:", err)
+		return
+	}
+	defer resposta.Body.Close()
+
+	var resposta_map map[string]interface{}
+	if err := json.NewDecoder(resposta.Body).Decode(&resposta_map); err != nil {
+		fmt.Println("Erro ao decodificar o JSON:", err)
+		return
+	}
+	resp_serv, ok := resposta_map["status"].(string)
+	if !ok {
+		fmt.Println("Ocorreu um erro")
+		return
+	}
+	fmt.Println(resp_serv)
+}
+
+// Função para requisição PATCH de cancelamento de rota
+func cancelar(id int, rota string) {
+	var cliente = Client_req{Id: id, Rota: rota}
+	json_valor, err := json.Marshal(cliente)
+	if err != nil {
+		fmt.Println("Erro ao serializar o JSON:", err)
 		return
 	}
 
-	fmt.Println("Resposta POST:", string(body))
+	req, err := http.NewRequest("PATCH", url+"/cancelar_rota", bytes.NewBuffer(json_valor))
+	if err != nil {
+		fmt.Println("Erro ao fazer a requisição PATCH:", err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	cliente_http := &http.Client{}
+	resposta, err := cliente_http.Do(req)
+	if err != nil {
+		fmt.Println("Erro ao fazer a requisição PATCH:", err)
+		return
+	}
+	defer resposta.Body.Close()
+
+	var resposta_map map[string]interface{}
+	if err := json.NewDecoder(resposta.Body).Decode(&resposta_map); err != nil {
+		fmt.Println("Erro ao decodificar o JSON:", err)
+		return
+	}
+	resp_serv, ok := resposta_map["status"].(string)
+	if !ok {
+		fmt.Println("Ocorreu um erro")
+		return
+	}
+	fmt.Println(resp_serv)
+}
+
+func main() {
+	fmt.Println("Digite o endereço do servidor:")
+	fmt.Scanln(&url)
+	var entrada string
+	var id int = -1
+	for id == -1 {
+		fmt.Println("Digite um nome de usuário:")
+		fmt.Scanln(&entrada)
+		id = cadastrar(entrada)
+	}
+
+	for {
+		fmt.Println("Conectando-se ao servidor", url)
+		for entrada != "5"{
+			fmt.Println("Digite um comando [1 - Rotas, 2 - Minhas Rotas, 3 - Comprar, 4 - Cancelar, 5 - Sair]:")
+			fmt.Scanln(&entrada)
+			switch entrada {
+				case "1":
+					rotas_recebidas := rotas_todas()
+					fmt.Println("Quantidade de rotas recebidas:", len(rotas_recebidas))
+					for rota, disp := range rotas_recebidas {
+						fmt.Print("Rota: ", rota, " => ")
+						if disp == 0 {
+							fmt.Println("Disponível")
+						} else {
+							fmt.Println("Indisponível")
+						}
+					}
+				case "2":
+					rotas_recebidas := rotas_cliente(id)
+					fmt.Println("Quantidade de rotas recebidas:", len(rotas_recebidas))
+					for _, rota := range rotas_recebidas {
+						fmt.Println("Rota:", rota)
+					}
+				case "3":
+					fmt.Println("Digite a rota que deseja comprar:")
+					fmt.Scanln(&entrada)
+					comprar(id, entrada)
+				case "4":
+					fmt.Println("Digite a rota que deseja cancelar:")
+					fmt.Scanln(&entrada)
+					cancelar(id, entrada)
+				case "5":
+					fmt.Println("Saindo...")
+				default:
+					fmt.Println("Comando inválido")
+			}
+		}
+		fmt.Println("Digite o endereço do servidor:")
+		fmt.Scanln(&url)
+		entrada = ""
+	}
 }
